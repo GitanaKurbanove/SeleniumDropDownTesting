b05a80fcd2984fca90a94f617748f3fddef7b987
GitanaKurbanove <gitana.kurbanove@gmail.com>
2020-06-02 21:58:45 +0300

    Add files via upload

---
+++ SelectDemoDropdownPage.cs
@@ -0,0 +1,83 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VcsWebdriver.Pages
+{
+    class SelectDemoDropdownPage: PageBase
+    {
+
+        private static SelectElement DienosPasirinkimoSarasas => new SelectElement(Driver.FindElement(By.Id("select-demo")));
+        private static IWebElement PasirinktosDienosPavadinimas => Driver.FindElement(By.ClassName("selected-value"));
+
+        private static SelectElement MiestuPasirinkimoSarasas => new SelectElement(Driver.FindElement(By.Id("multi-select")));
+        private static IWebElement GetAllSelectedMygtukas => Driver.FindElement(By.Id("printAll"));
+        private static IWebElement RezultatoZinute => Driver.FindElement(By.ClassName("getall-selected"));
+
+
+        public SelectDemoDropdownPage(IWebDriver _driver) : base(_driver)
+        {
+            Driver.Url = "https://www.seleniumeasy.com/test/basic-select-dropdown-demo.html";
+        }
+
+        public SelectDemoDropdownPage PasirinktaDiena(DayOfWeek dayOfWeek)
+        {
+            DienosPasirinkimoSarasas.SelectByValue(dayOfWeek.ToString());
+            return this;
+        }
+
+        public SelectDemoDropdownPage PatikrintiPasirinktaDiena(DayOfWeek tiketinasPasirinkimas)
+        {
+            Assert.AreEqual($"Day selected :- {tiketinasPasirinkimas}", PasirinktosDienosPavadinimas.Text);
+            return this;
+        }
+
+        public SelectDemoDropdownPage PasirinkReiksmeIsDidelioLauko(string reiksme)
+        {
+            MiestuPasirinkimoSarasas.SelectByValue(reiksme);
+            return this;
+        }
+
+        public SelectDemoDropdownPage PasirinkReiksmesIsDidelioLauko(string[] elementuValuesKuriuosPasirinksime)
+        {
+            var actions = new Actions(Driver).KeyDown(Keys.LeftControl);
+
+            foreach (var option in MiestuPasirinkimoSarasas.Options)
+                if (elementuValuesKuriuosPasirinksime.Contains(option.GetAttribute("value")))
+                    actions.Click(option);
+
+            actions.KeyUp(Keys.LeftControl).Build().Perform();
+            return this;
+        }
+
+        public SelectDemoDropdownPage PaspauskGetAllSelected()
+        {
+            GetAllSelectedMygtukas.Click();
+            return this;
+        }
+
+        public SelectDemoDropdownPage TikrinkPasirinktasReiksmes(string[] elementuValuesKuriuosPasirinksime)
+        {
+            string message = RezultatoZinute.Text;
+            Assert.IsTrue(message.StartsWith("Options selected are : "), "Expected message to start with 'Options selected are : '");
+            foreach (var reiksme in elementuValuesKuriuosPasirinksime)
+                Assert.IsTrue(message.Contains(reiksme), $"Expected '{message}' to contain '{reiksme}'");
+
+            return this;
+        }
+
+        public SelectDemoDropdownPage TikrinkPasirinktaReiksme(string reiksme)
+        {
+            string message = RezultatoZinute.Text;
+            Assert.AreEqual($"Options selected are : {reiksme}", message, "Uzrasas nesutampa");
+            return this;
+        }
+    }
+}
---
+++ SelectDemoPageDropdownTest.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VcsWebdriver.Drivers;
+using VcsWebdriver.Pages;
+
+namespace VcsWebdriver.Tests
+{
+    class SelectDemoPageDropdownTest
+    {
+
+        private static SelectDemoDropdownPage _page;
+
+        [OneTimeSetUp]
+
+        public static void SetUpChrome()
+        {
+            var driver = CustomDrivers.GetChromeDriver();
+            _page = new SelectDemoDropdownPage(driver);
+        }
+
+        [TestCase(DayOfWeek.Tuesday)]
+      
+        public static void DayOfWeekDropDown(DayOfWeek testuojamaDiena)
+        {
+            _page.PasirinktaDiena(testuojamaDiena).PatikrintiPasirinktaDiena(testuojamaDiena);
+        }
+
+        [TestCase("New York", TestName = "Pasirenkame viena reiksme ir patikriname")]
+        [TestCase("New Jersey", TestName = "Pasirenkame kita reiksme ir patikriname")]
+        public static void PasirinktaVienaReiksme(string elementoValueKuriPasirinksime)
+        {
+            _page.PasirinkReiksmeIsDidelioLauko(elementoValueKuriPasirinksime)
+                .PaspauskGetAllSelected()
+                .TikrinkPasirinktaReiksme(elementoValueKuriPasirinksime);
+        }
+
+        [TestCase("New Jersey", "California", TestName = "Pasirenkame 2 reiksmes ir patikriname")]
+        [TestCase("Washington", "Ohio", "Texas", TestName = "Pasirenkame 3 reiksmes ir patikriname")]
+        [TestCase("Washington", "Ohio", "Texas", "Florida", TestName = "Pasirenkame 4 reiksmes ir patikriname")]
+        public static void PasirinktiKeliasReiksmes(params string[] elementuValuesKuriuosPasirinksime)
+        {
+            _page.PasirinkReiksmesIsDidelioLauko(elementuValuesKuriuosPasirinksime)
+                .PaspauskGetAllSelected()
+                .TikrinkPasirinktasReiksmes(elementuValuesKuriuosPasirinksime);
+        }
+
+        [OneTimeTearDown]
+        public static void CloseBroser()
+        {
+            _page.CloseBrowser();
+        }
+    }
+}
